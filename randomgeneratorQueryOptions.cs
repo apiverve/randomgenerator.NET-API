0f8e5d8d5fc5149875a3643abb0a6ec69bee0785
APIVerve Team <hello@apiverve.com>
2024-05-14 15:38:23 -0500

    Bug fixes and Improvements

---
+++ randomgeneratorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class randomgeneratorQueryOptions {
+public string type { get; set; }
+public int count { get; set; }
+}
+
+}
